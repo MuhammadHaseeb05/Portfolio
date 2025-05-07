f4352e034f0f90c34d674685a7a47dbc2c5ddc8e
HaseebJ05 <m.haseebjanjua2005@gmail.com>
2025-05-07 20:44:00 +0500

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Typography variant="h6" component="div">
+          My Portfolio
+        </Typography>
+        <Box>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Projects</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
